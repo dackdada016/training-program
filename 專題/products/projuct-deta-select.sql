f354585684001a13fd57aaf6ef66d024bb37513f
Yun-Rou <dackdada016@gmail.com>
2022-12-18 13:41:13 +0800

    商品詳細搜尋語法錯誤更新

--- 專題/products/projuct-deta-select.sql
+++ 專題/products/projuct-deta-select.sql
@@ -1,8 +1,8 @@
 -- 商品總覽 
 select * FROM `products` LIMIT 0, 10;
 -- 查看商品詳細內容
-select * FROM `products` WHERE `products_name`;
-
+-- 錯的 select * FROM `products` WHERE `products_name`;
+select * FROM products WHERE `products_id`=1;
 -- 搜尋這個會員的購物車清單
 select `products_id` FROM `shopping_cart` WHERE `member_id`=1;
 
