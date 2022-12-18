4e2a54ccde06f017c188458ba0804f7cbb9f7602
Yun-Rou <dackdada016@gmail.com>
2022-12-18 23:44:02 +0800

    LIMIT 修改

--- 專題/products/projuct-deta-select.sql
+++ 專題/products/projuct-deta-select.sql
@@ -1,8 +1,9 @@
 -- 商品總覽 
-select * FROM `products` LIMIT 0, 10;
+select * FROM `products` LIMIT top, 10;
 -- 查看商品詳細內容
 -- 錯的 select * FROM `products` WHERE `products_name`;
 select * FROM products WHERE `products_id`=1;
+
 -- 搜尋這個會員的購物車清單
 select `products_id` FROM `shopping_cart` WHERE `member_id`=1;
 
