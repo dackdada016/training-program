031cecd10129bbc160870fd1521c599f8b072174
YunRouNing <dackdada016@gmail.com>
2022-12-20 14:47:03 +0800

    首頁轉向及資料庫連線

---
+++ 期中專題/index_.php
@@ -0,0 +1,10 @@
+<?php
+require './product_manage/part/connect.php';
+
+?>
+<?php include './product_manage/part/html-header.php' ?>
+<?php include './product_manage/part/html-body-header.php' ?>
+<?php include './product_manage/part/html-body-sidebar.php' ?>
+<?php include './product_manage/part/html-body-nav.php' ?>
+<?php include './product_manage/part/html-script.php' ?>
+<?php include './product_manage/part/html-foot.php' ?>
