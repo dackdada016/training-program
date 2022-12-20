ee706dc9020933240ee52e6abec1d2fb0e84b22c
YunRouNing <dackdada016@gmail.com>
2022-12-20 14:49:54 +0800

    sidbar功能

---
+++ 期中專題/product_manage/part/html-body-sidebar.php
@@ -0,0 +1,17 @@
+<container class="d-flex border">
+    <div class="sidebar border">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">會員管理</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">訂單管理</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  disabled" href="#">商品管理</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">住宿管理</a>
+        </li>
+      </ul>
+    </div>
