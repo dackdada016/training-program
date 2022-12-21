b72eec07177b2418f23a36196935e56921cbd96f
YunRouNing <dackdada016@gmail.com>
2022-12-21 15:13:38 +0800

    調整縮排

--- 期中專題/product_manage/part/html-body-header.php
+++ 期中專題/product_manage/part/html-body-header.php
@@ -3,7 +3,8 @@
       <div class="col-10">
         <h3 class="m-1">管理系統</h3>
       </div>
-      <div class="col-1"><p class="m-0 text-centent">HI,user</p></div>
+      <div class="col-1"><p class="m-0 text-centent">HI,user
+      </p></div>
       <div class="col-1"><i class="fa-solid fa-right-from-bracket"></i></div>
     </div>
   </header>
