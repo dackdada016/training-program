7698da41127b841aa82d4ebdefca5b316ce72bc1
YunRouNing <dackdada016@gmail.com>
2022-12-23 11:32:37 +0800

    移除$output重複的內容 新增檔案上傳功能

--- 期中專題/product_manage/add_api.php
+++ 期中專題/product_manage/add_api.php
@@ -7,16 +7,57 @@ $output = [
   'lastId' => 0,
   'postData' => $_POST,
   'code' => 0,
-  'errors' => []
+  'errors' => '',
+  'filename' => '',
 ];
 
-if(empty($_POST['productName'])) {
-  $output['errors']['productName'] = '沒有商品名稱';
-  echo json_encode($output, JSON_UNESCAPED_UNICODE);
-  exit;
+
+
+// TODO:檔案上傳
+
+// 篩選檔案類型, 決定副檔名
+$extMap = [
+  'image/jpeg' => '.jpg',
+  'image/jpg' => '.jpg',
+  'image/png' => '.png',
+];
+
+$path = __DIR__. './上傳照片/';
+
+if(! empty($_FILES['my_file'])){
+  $f = $_FILES['my_file'];
+
+  $ext = isset($extMap[$f['type']]) ? $extMap[$f['type']] : ''; // 決定副檔名
+  if(empty($ext)){
+    // 檔案類型是錯的
+    $output['error'] = '檔案類型錯誤';
+  } else {
+    $fname = sha1($f['name']. rand()). $ext; // 檔案名稱, 不包含路徑
+
+    if(move_uploaded_file(
+      $f['tmp_name'],
+      $path. $fname
+    )){
+      $output['success'] = true;
+      $output['filename'] = $fname;
+    } else {
+      $output['error'] = '無法搬移檔案';
+    }
+  }
 }
 
-// TODO: 欄位資料檢查
+
+
+// if(empty($_POST['productName'])) {
+//   $output['errors']['productName'] = '沒有商品名稱';
+//   echo json_encode($output, JSON_UNESCAPED_UNICODE);
+//   exit;
+// }
+
+// // TODO: 欄位資料檢查
+
+
+
 
 $isPass = true; // 是否通過檢查
 
@@ -26,7 +67,7 @@ $productsType = $_POST['productsType'] ?? '';
 $productsDecripttion = $_POST['productsDecripttion'] ?? '';
 $productPrice = $_POST['productPrice'] ?? '';
 $productUnit = $_POST['productUnit'] ?? '';
-$my_file = $_FILES['my_file']['name'] ?? '';
+//$my_file = $_FILES['my_file']['name'] ?? '';
 
 
 
@@ -34,20 +75,10 @@ if(mb_strlen($productName) < 2){
   $output['errors']['productName'] = '請填寫商品名稱';
   $isPass = false;
 }
+// TODO:寫進資料庫
 
-// if(filter_var($email, FILTER_VALIDATE_EMAIL)===false){
-//   $output['errors']['email'] = '請填寫正確的 email';
-//   $isPass = false;
-// }
-
-
-// $sql = " INSERT INTO `products`(`products_id`, `products_name`, `type_id`, `products_decripttion`, `products_price`, `products_unit`, `products_img_name`) VALUES (?,?,?,?,?,?,?) " and " INSERT INTO `product_imgs`(`products_id`, `img_name`) VALUES (?,?)";
 $sql = " INSERT INTO `products`(`products_name`, `type_id`, `products_decripttion`, `products_price`, `products_unit`, `products_img_name`) VALUES (?,?,?,?,?,?)";
-
   // and " INSERT INTO `product_imgs`(`products_id`, `img_name`) VALUES (?,?)";
-
-
-
 $stmt = $pdo->prepare($sql);
 
 if($isPass) {
@@ -57,7 +88,7 @@ if($isPass) {
     $productsDecripttion,
     $productPrice,
     $productUnit,
-    $my_file,
+    $fname,
   ]);
 
   // lastInsertId -> 得到前面新增的資料(最新一筆)的id
@@ -66,15 +97,9 @@ if($isPass) {
   $output['success'] = !! $stmt->rowCount();
 
 }
-$sql_img = " INSERT INTO `product_imgs`(`products_id, `img_name`) VALUES (?,?) ";
 
-$stmt_img = $pdo->prepare($sql_img);
-if($isPass){
-  $stmt_img->execute([
-    $id,
-    $my_file,
-  ]);
-}
 
+
+// echo json_encode($output);
 
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
