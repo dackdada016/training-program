6ae7c6e4798bca2d9fe2139453aec68f8c281a07
YunRouNing <dackdada016@gmail.com>
2022-12-24 16:54:47 +0800

    建立刪除功能function並修改table hover的樣式

--- 期中專題/product_manage/list.php
+++ 期中專題/product_manage/list.php
@@ -21,39 +21,64 @@ $rows = $pdo->query($sql)->fetchAll();
           </div>
           
         </div>
-        <table class="table table-striped table align-middle">
+        <table class="table table-hover align-middle">
             <thead>
                 <tr>
                     <th scope="col">圖片</th>
-                    <th scope="col">商品ID</th>
+                    <th scope="col" hidden>商品ID</th>
                     <th scope="col-1">商品名稱</th>
                     <th scope="col-1">商品類型</th>
                     <th scope="col-1">商品介紹</th>
                     <th scope="col-1">商品價格</th>
                     <th scope="col-1">販售單位</th>
-                    <th scope="col">操作</th>
+                    <th scope="col-2" colspan="2" class="text-center">操作</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($rows as $r) :?>
                 <tr>
                     <td><img src="<?= $path . $r['products_img_name'] ?>" width="90" height="90"></td>
-                    <td><?= $r['products_id'] ?></td>
-                    <td><?= $r['products_name'] ?></td>
+                    <td hidden><?= $r['products_id'] ?></td>
+                    <td ><?= $r['products_name'] ?></td>
                     <td><?= $r['type_id'] ?></td>
                     <td><?= $r['products_decripttion'] ?></td>
                     <td><?= $r['products_price'] ?></td>
                     <td><?= $r['products_unit'] ?></td>
                     <td class="align-items-center">
-                        <a href="<?= "updata.php?products_id=" . $r['products_id'] ?>" ><i class="fa-solid fa-pen-to-square btn border">編輯</i></a>
-                        <i class="fa-solid fa-trash-can btn border">刪除</i>
+                        <a href="<?= "updata.php?products_id=" . $r['products_id'] ?>" >
+                            <i class="fa-solid fa-pen-to-square btn border">編輯</i>
+                        </a>
+                    </td>
+                    <td class="align-items-center">
+                        <a href="javascript: delete_it(<?= $r['products_id'] ?>)">
+                            <i class="fa-solid fa-trash-can btn border">刪除</i>
+                        </a>    
                     </td>
                 </tr>
             </tbody>
             <?php endforeach ?>
         </table>
+        <ul class="pagination justify-content-end">
+            <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+            </li>
+        </ul>
     </nav>
 </container>
 
 <?php include './part/html-script.php' ?>
+<script>
+  function delete_it(products_id){
+    if(confirm(`確定要刪除編號為 ${products_id} 的資料嗎?`)){
+      location.href = `delete.php?products_id=${products_id}`;
+    }
+  }
+
+</script>
 <?php include './part/html-foot.php' ?>
