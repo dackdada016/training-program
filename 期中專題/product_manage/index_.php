f37a0907a440535a597c52e648551667550c044c
YunRouNing <dackdada016@gmail.com>
2022-12-26 11:45:23 +0800

    首頁調整直接查看商品list

--- 期中專題/product_manage/index_.php
+++ 期中專題/product_manage/index_.php
@@ -4,7 +4,7 @@ require './part/connect_db.php';
 ?>
 <?php include './part/html-header.php' ?>
 <?php include './part/html-body-header.php' ?>
-<?php include './part/html-body-sidebar.php' ?>
+<?php include './list.php' ?>
 <?php include './part/html-body-nav.php' ?>
 <?php include './part/html-script.php' ?>
 <?php include './part/html-foot.php' ?>
