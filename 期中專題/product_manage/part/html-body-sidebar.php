bdc566f8487225e857042b9d814dc9a004aa0e68
YunRouNing <dackdada016@gmail.com>
2022-12-26 11:41:40 +0800

    欄位名稱刪除管理字樣

--- 期中專題/product_manage/part/html-body-sidebar.php
+++ 期中專題/product_manage/part/html-body-sidebar.php
@@ -2,16 +2,16 @@
     <div class="sidebar border">
       <ul class="nav flex-column">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">會員管理</a>
+          <a class="nav-link active" aria-current="page" href="#">會員</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">訂單管理</a>
+          <a class="nav-link" href="#">訂單</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="list.php" disabled>商品管理</a>
+          <a class="nav-link" href="list.php" disabled>商品</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link">住宿管理</a>
+          <a class="nav-link">住宿</a>
         </li>
       </ul>
     </div>
